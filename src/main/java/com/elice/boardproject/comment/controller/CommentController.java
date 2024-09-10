58757c467ad6a65d9128e3a9dc55257344b8c3b3
Doohyun Kim <engus7303>
2024-09-11 01:03:25 +0900

    fix:댓글 수정,삭제

--- src/main/java/com/elice/boardproject/comment/controller/CommentController.java
+++ src/main/java/com/elice/boardproject/comment/controller/CommentController.java
@@ -13,10 +13,26 @@ public class CommentController {
     private final CommentService commentService;
 
     // 댓글 작성
-    @PostMapping("/addComment")
+    @PostMapping("/{postId}/addComment")
     public String addComment(@PathVariable Long postId, @RequestBody CommentRequestDTO commentRequestDTO) {
         commentService.addComment(postId, commentRequestDTO);
 
-        return "post/createPost";
+        return "post/createComment";
+    }
+
+    // 댓글 수정
+    @PatchMapping("/{commentId}/updateComment")
+    public String updateComment(@PathVariable Long commentId, @RequestBody CommentRequestDTO commentRequestDTO) {
+        commentService.updateComment(commentId, commentRequestDTO);
+
+        return "post/editComment";
+    }
+
+    // 댓글 삭제
+    @DeleteMapping("/{commentId}/deleteComment")
+    public String deleteComment(@PathVariable Long commentId) {
+        commentService.deleteComment(commentId);
+
+        return "post/deleteComment";
     }
 }
